6dd7e79cb317533bfd2352df91cfaaf455a15210
mkgask <mmp@zsw.jp>
2024-11-02 02:08:17 +0900

    Adjust Layer Edit Icon

--- src/components/LayerList.tsx
+++ src/components/LayerList.tsx
@@ -40,13 +40,13 @@ const LayerList: Component = () => {
           <li class="p-2 border-b dark:border-gray-600 flex justify-between items-center">
             <span>{layer.name} ({layer.type})</span>
             <button
-              class="p-1 bg-yellow-500 text-white rounded-full hover:bg-yellow-600"
+              class="p-1 text-blue"
               onClick={() => {
                 setSelectedLayer(layer);
                 setIsEditDialogOpen(true);
               }}
             >
-              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <svg class="w-4 h-4 text-gray-400 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 20h9M16.5 3.5a2.121 2.121 0 1 1 3 3L7 19l-4 1 1-4 12.5-12.5Z"/>
               </svg>
             </button>
