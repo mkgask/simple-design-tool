189e7e999fa70a0769c77586107f7882c868426a
mkgask <mmp@zsw.jp>
2024-11-02 00:44:32 +0900

    Adjust layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,16 +6,18 @@ import LayerList from './LayerList';
 const Main: Component = () => {
   return (
     <div class="flex">
-      <div class="flex flex-col w-2/3 p-4">
-        <div class="mb-4">
+      <div class="flex flex-col w-2/3">
+        <div class="p-4 border-b dark:border-gray-600">
           <Toolbar />
         </div>
-        <div class="flex-1 mb-4">
+        <div class="flex-1 p-4">
           <Canvas />
         </div>
       </div>
-      <div class="w-1/3 p-4">
-        <LayerList />
+      <div class="w-1/3 border-l dark:border-gray-600">
+		<div class="p-4">
+			<LayerList />
+		</div>
       </div>
     </div>
   );
